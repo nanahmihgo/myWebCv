f122bf6d02f16d3045cb846bc3de0542a1a6d662
Vincent <vince.dev77@gmail.com>
2021-11-12 17:26:55 +0100

    1st commit

---
+++ js/app.js
@@ -0,0 +1,47 @@
+//Objet Vince
+
+// let Vince = {
+//     name : "Fischer",
+//     firstName: "Vince",
+//     age: 37,
+//     sexe: Male,
+//     job: "Développeur web",
+//     passion: "Physiologie",
+//     hobbies: "Sport and e-sport"
+// };
+
+class Profil {
+    constructor(name, firstName, age, sexe, job, passion, hobbies) {
+        this.name = name;
+        this.firstName = firstName;
+        this.age = age;
+        this.sexe = sexe;
+        this.job = job;
+        this.passion = passion;
+        this.hobbies = hobbies;
+    }
+}
+
+// class & new
+let Vince = new Profil("Fischer", "Vince", 37, Male, "Développeur web", "Physiologie", "Sport and e-sport");
+let Jamal = new Profil("Taoufiki", "Jamal", 37, Male, "Développeur web", "Pizzas", "Sieste");
+let xbv = new Profil("Bourgeois", "Xavier", 37, Male, "Web designer", "Politique", "Design");
+
+// Variables
+let noir = #000000;
+
+//Condition de connexion
+// let UserLoggedIn = true;
+// if (UserLoggedIn) {
+//    console.log("Utilisateur connecté!");
+// } else {
+//    console.log("Alerte, intrus!");
+// }
+
+
+// SON GUN !
+bouton.onclick=function(){
+    son1.play();
+    };
+
+
