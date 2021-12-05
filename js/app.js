b3c7cf701c5fae2994c39299c67a9893ec0e1ec2
Vincent <vince.dev77@gmail.com>
2021-12-05 02:55:53 +0100

    menu/register

--- js/app.js
+++ js/app.js
@@ -1,3 +1,63 @@
+// SON GUN !
+bouton.onclick=function(){
+    son1.play()
+    }
+
+
+let options = document.querySelectorAll('option')
+
+ options[0].addEventListener('click', () => {
+   window.location.href='./html/inscr.html'
+ })
+ options[1].addEventListener('click', () => {
+   window.location.href='./html/inscr.html'
+ })
+ options[2].addEventListener('click', () => {
+   window.location.href='./html/inscr.html'
+ })
+
+// console.log(options)
+// options.addEventListener('click', () => {
+//     switch(options) {
+//     case options[0]: 
+//     window.location.href='./html/inscr.html'
+//     case options[1]: 
+//     window.location.href='./html/inscr.html'
+//     case options[2]: 
+//     window.location.href='./html/inscr.html'
+//     breack
+
+//     default: console.log('va te faire foutre !')
+//     }
+//   })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 //Objet Vince
 
 // let Vince = {
@@ -10,25 +70,38 @@
 //     hobbies: "Sport and e-sport"
 // };
 
-class Profil {
-    constructor(name, firstName, age, sexe, job, passion, hobbies) {
-        this.name = name;
-        this.firstName = firstName;
-        this.age = age;
-        this.sexe = sexe;
-        this.job = job;
-        this.passion = passion;
-        this.hobbies = hobbies;
-    }
-}
+// class Profil {
+//      (name, firstName, age, sexe, job, passion, hobbies) {
+//         this.name = name;
+//         this.firstName = firstName;
+//         this.age = age;
+//         this.sexe = sexe;
+//         this.job = job;
+//         this.passion = passion;
+//         this.hobbies = hobbies;
+//     }
+// }
+
+// function Voiture (motorisation, annee, model) {
+//        this.motorisation = motorisation,
+//        this.annee = annee,
+//        this.model = model
+//    }
 
 // class & new
-let Vince = new Profil("Fischer", "Vince", 37, Male, "Développeur web", "Physiologie", "Sport and e-sport");
-let Jamal = new Profil("Taoufiki", "Jamal", 37, Male, "Développeur web", "Pizzas", "Sieste");
-let xbv = new Profil("Bourgeois", "Xavier", 37, Male, "Web designer", "Politique", "Design");
+// let seat = new voiture(essence, 2021, ibiza);
+// let bmw = new voiture(diesel, 2021, m3);
+// let dacia = new voiture(essence, 2014, sandero);
+// let Jamal = new Profil("Taoufiki", "Jamal", 37, Male, "Développeur web", "Pizzas", "Sieste");
+// let xbv = new Profil("Bourgeois", "Xavier", 37, Male, "Web designer", "Politique", "Design");
 
-// Variables
-let noir = #000000;
+// class & new
+// let Vince = new Profil("Fischer", "Vince", 37, Male, "Développeur web", "Physiologie", "Sport and e-sport");
+// let Jamal = new Profil("Taoufiki", "Jamal", 37, Male, "Développeur web", "Pizzas", "Sieste");
+// let xbv = new Profil("Bourgeois", "Xavier", 37, Male, "Web designer", "Politique", "Design");
+
+// // Variables
+// let noir = #000000;
 
 //Condition de connexion
 // let UserLoggedIn = true;
@@ -39,9 +112,13 @@ let noir = #000000;
 // }
 
 
-// SON GUN !
-bouton.onclick=function(){
-    son1.play();
-    };
+//UNE BELLE FONCTION !!!!!
 
-
+// const nomDeFunction = (a, b) => {
+//     return a+b;
+//   }
+  
+//   let premier = 1;
+//   let second = 2;
+  
+//   let result = nomDeFunction(premier, second);
